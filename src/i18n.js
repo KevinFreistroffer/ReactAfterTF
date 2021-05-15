0028acf6db48bd9ebc6975450bd262eba2c17b0e
Kevin <kevin.freistroffer@gmail.com>
2021-05-15 06:38:08 -0700

    load:currentlyOnly

--- src/i18n.js
+++ src/i18n.js
@@ -23,6 +23,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({ 
+    load: "currentOnly",
     detection: {
       order: [ 'cookie', 'localStorage', 'navigator', 'htmlTag'],
       lookupCookie: 'LANG_CODE',
